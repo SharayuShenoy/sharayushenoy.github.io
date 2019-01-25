449169d4dd746830b66f390b83717cfdccdf7439
SharayuShenoy <43452306+SharayuShenoy@users.noreply.github.com>
2019-01-25 02:58:15 -0600

    Create rp.js

---
+++ rp.js
@@ -0,0 +1,98 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="Sharayu Shenoy, UT Austin, University of Texas at Austin, Portfolio, Website">
+    <meta name="author" content="Sharayu Shenoy">
+    <meta name="generator" content="Jekyll v3.8.5">
+    <title>Sharayu Shenoy</title>
+
+    <!-- Bootstrap core CSS -->
+<link href="https://getbootstrap.com/docs/4.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
+
+    <!-- Custom styles for this template -->
+    <link href="custom.css" rel="stylesheet">
+  </head>
+  <body class="">
+        <div class="custom-header shadow-sm d-none d-md-block"></div>
+    <div class="col-md-8 d-flex w-100 h-100 p-3 mx-auto flex-column">
+  <header class="masthead mb-4">
+    <div class="inner">
+      <h3 class="masthead-brand">SHARAYU SHENOY</h3>
+      <nav class="nav nav-masthead justify-content-center">
+        <a class="nav-link" href="index.html">Home</a>
+        <a class="nav-link active" href="projects.html">Projects</a>
+        <a class="nav-link" href="about.html">About</a>
+      </nav>
+    </div>
+
+  </header>
+
+  <main role="main" class="inner cover">
+    <h1 class="cover-heading display-3 mt-5 mb-5">Prevent Opioid Overdose</h1>
+    
+    <div class="row featurette">
+      <div class="col-md-12">
+        <h2 class="featurette-heading">Concept</h2>
+        <p class="lead">
+          
+        </p>
+      </div>
+      
+    </div>
+    <hr class="featurette-divider">    
+
+    <div class="row featurette">
+      <div class="col-md-7">
+        <h2 class="featurette-heading">Actors </h2>
+        <p class="lead">
+          
+        </p>
+      </div>
+      <div class="col-md-5">
+        <img src="img/demo.jpg" alt="demographics" width="100%">
+      </div>
+    </div>
+    <hr class="featurette-divider">
+    <div class="row featurette">
+      <div class="col-md-7">
+        <img src="img/waad.jpg" alt="WAAD" width="100%">
+      </div>
+      <div class="col-md-5">
+        <h2 class="featurette-heading">User Scenario 1</h2>
+        <p class="lead">
+          System detects/suspects an overdose
+        </p>
+      </div>
+    </div>
+    <hr class="featurette-divider">
+        <div class="row featurette">
+      <div class="col-md-5">
+        <h2 class="featurette-heading">User Scenario 2</h2>
+        <p class="lead">
+        Help user stay focused and on-track
+        </p>
+      </div>
+      <div class="col-md-7">
+        <img src="img/usageflow.jpg" alt="usage flow model" width="100%">
+      </div>
+    </div>
+    <hr class="featurette-divider">
+    <div class="row featurette">
+      <div class="col-md-7">
+        <img src="img/story.jpg" alt="demographics" width="100%">
+      </div>
+      <div class="col-md-5">
+        <h2 class="featurette-heading">User Scenario 3</h2>
+        <p class="lead">Medical provider wishes to check patients progress</p>
+      </div>
+    </div>
+    <hr class="featurette-divider">
+    <h2 class="featurette-heading">Final Product</h2>    
+    <hr class="featurette-divider">
+  </main>
+
+</div>
+</body>
+</html>
