a437fce79bffe880da4081c911d06e1f213b893b
sharayushenoy <sharayushenoy92@gmail.com>
2019-09-10 02:48:41 -0500

    update

--- app.js
+++
@@ -1,3 +0,0 @@
-import { util } from './js/util.js';
-import { index } from './js/index.js';
-import { home } from './js/home.js';
---
+++ js/filter.js
@@ -0,0 +1,52 @@
+var mytext = getUrlParam('type','all');
+
+function getUrlParam(parameter, defaultvalue){
+    var urlparameter = defaultvalue;
+    if(window.location.href.indexOf(parameter) > -1){
+        urlparameter = getUrlVars()[parameter];
+        }
+    filterSelection(urlparameter);
+}
+
+function getUrlVars() {
+    var vars = {};
+    var parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function(m,key,value) {
+        vars[key] = value;
+    });
+    return vars;
+}
+
+function filterSelection(c) {
+  var x, i, p;
+  x = document.getElementsByClassName("filterDiv");
+  // p = document.getElementsByClassName(c);
+  if (c == "all") c = "";
+  for (i = 0; i < x.length; i++) {
+    w3RemoveClass(x[i], "show");
+    if (x[i].className.indexOf(c) > -1){
+      w3AddClass(x[i], "show");
+      // w3AddClass(p[0], "show")
+    }
+  }
+}
+
+function w3AddClass(element, name) {
+  var i, arr1, arr2;
+  arr1 = element.className.split(" ");
+  arr2 = name.split(" ");
+  for (i = 0; i < arr2.length; i++) {
+    if (arr1.indexOf(arr2[i]) == -1) {element.className += " " + arr2[i];}
+  }
+}
+
+function w3RemoveClass(element, name) {
+  var i, arr1, arr2;
+  arr1 = element.className.split(" ");
+  arr2 = name.split(" ");
+  for (i = 0; i < arr2.length; i++) {
+    while (arr1.indexOf(arr2[i]) > -1) {
+      arr1.splice(arr1.indexOf(arr2[i]), 1);
+    }
+  }
+  element.className = arr1.join(" ");
+}
